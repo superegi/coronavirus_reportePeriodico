3876a3586bcc4149e3572b978363eff77d9b654e
superegi <50183741+superegi@users.noreply.github.com>
2022-03-03 00:07:46 -0300

    Update main.py

--- Scraping_epivigila/main.py
+++ Scraping_epivigila/main.py
@@ -1,4 +1,5 @@
 import requests
+import subprocess
 
 
 print('Consiguiendo ultima version')
@@ -6,4 +7,6 @@ print('Consiguiendo ultima version')
 url = "https://raw.githubusercontent.com/superegi/coronavirus_reportePeriodico/main/Scraping_epivigila/scrap_current.py"
 resp = requests.get(url)
 with open("scrap_current.py", "w") as f:
-    f.write(resp.text)
+     f.write(resp.text)
+
+subprocess.call(["python", "scrap_current.py"])
