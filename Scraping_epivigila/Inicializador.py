71f2243d2ac8da84d01f9fded3058c1780cc00e5
ViZenn <86311653+ViZenn@users.noreply.github.com>
2022-06-01 23:10:57 -0400

    v2.0 descarga 2 archivos y ejecuta main

--- Scraping_epivigila/Inicializador.py
+++ Scraping_epivigila/Inicializador.py
@@ -5,15 +5,15 @@ import subprocess
 
 print('Consiguiendo ultima version')
 
+url = "http://raw.githubusercontent.com/superegi/coronavirus_reportePeriodico/main/Scraping_epivigila/Funciones_Scraping.py"
+resp = requests.get(url)
+with open("Funciones_Scraping.py", "w", encoding="utf-8") as f:
+     f.write(resp.text)
+
 url = "http://raw.githubusercontent.com/superegi/coronavirus_reportePeriodico/main/Scraping_epivigila/MAIN.py"
 resp = requests.get(url)
 with open("MAIN.py", "w" ,encoding="utf-8") as f:
      f.write(resp.text)
 
-url = "http://raw.githubusercontent.com/superegi/coronavirus_reportePeriodico/main/Scraping_epivigila/Funciones_Scraping.py"
-resp = requests.get(url)
-with open("Funciones_Scraping.py", "w", encoding="utf-8") as f:
-     f.write(resp.text)
-     
 subprocess.call(["python", "MAIN.py"])
 print('listo')
